3d1ec80820ce07679cbbf361ca386c6227cdf74a
John Everton <johneverton44@gmail.com>
2024-07-15 18:07:14 -0300

    feat: create table header

--- web/src/components/attendee-list.tsx
+++ web/src/components/attendee-list.tsx
@@ -1,3 +1,18 @@
+import { Search } from 'lucide-react'
+
 export function AttendeeList() {
-  return <h1>AttendeeList</h1>
+  return (
+    <div className="flex items-center gap-3">
+      <h1 className="text-2xl font-bold">Participantes</h1>
+      <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex gap-3 items-center">
+        <Search className="size-4 text-emerald-300" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          placeholder="Buscar participante..."
+          type="search" 
+        />
+
+      </div>
+    </div>
+)
 }
